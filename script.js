e3b91a84bbdbff451c09b236d8e8a10a1b69e32c
David Gibbons <gibbonsdav@gmail.com>
2020-07-24 10:08:02 -0700

    paddle and bricks

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const rules = document.getElementById("rules")
 const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
 
+let score = 0
+
+// Making the ball
 const ball = {
   x: canvas.width / 2,
   y: canvas.height / 2,
@@ -12,6 +15,47 @@ const ball = {
   dx: 4,
   dy: -4,
 }
+
+// Making the paddle
+const paddle = {
+  x: canvas.width / 2 - 40,
+  y: canvas.height - 20,
+  w: 80,
+  h: 10,
+  speed: 8,
+  dx: 0,
+}
+
+// Drawing the ball
+function drawBall() {
+  ctx.beginPath()
+  ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2)
+  ctx.fillStyle = "#0095dd"
+  ctx.fill()
+  ctx.closePath()
+}
+
+// draw the paddle
+function drawPaddle() {
+  ctx.beginPath()
+  ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h)
+  ctx.fillStyle = "#0095dd"
+  ctx.fill()
+  ctx.closePath
+}
+
+function draw() {
+  drawBall()
+  drawPaddle()
+  drawScore()
+}
+
+//Draw score in canvas
+function drawScore() {
+  ctx.font = "20px Arial"
+  ctx.fillText(`Score: ${score}`, canvas.width - 100, 30)
+}
+draw()
 
 //Rules and closing event handlers
 rulesBtn.addEventListener("click", () => rules.classList.add("show"))
