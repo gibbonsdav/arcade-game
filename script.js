aff1d782d2050b50182943623f8f16398e8b5329
David Gibbons <gibbonsdav@gmail.com>
2020-07-30 09:51:55 -0700

    changing score

--- script.js
+++ script.js
@@ -103,15 +103,6 @@ function movePaddle() {
   }
 }
 
-function draw() {
-  //clear canvas
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
-  drawBall()
-  drawPaddle()
-  drawScore()
-  drawBricks()
-}
-
 //move ball on canvas
 function moveBall() {
   ball.x += ball.dx
@@ -156,12 +147,29 @@ function moveBall() {
   })
 }
 
-//increase the score
+// Hit bottom wall - Lose
+if (ball.y + ball.size > canvas.height) {
+  showAllBricks()
+  score = 0
+}
+
+// Increase score
 function increaseScore() {
   score++
-
   if (score % (brickRowCount * brickColumnCount) === 0) {
-    showAllBricks()
+    ball.visible = false
+    paddle.visible = false
+    //After 0.5 sec restart the game
+    setTimeout(function () {
+      showAllBricks()
+      score = 0
+      paddle.x = canvas.width / 2 - 40
+      paddle.y = canvas.height - 20
+      ball.x = canvas.width / 2
+      ball.y = canvas.height / 2
+      ball.visible = true
+      paddle.visible = true
+    }, delay)
   }
 }
 
@@ -170,6 +178,15 @@ function showAllBricks() {
   bricks.forEach((column) => {
     column.forEach((brick) => (brick.visible = true))
   })
+}
+
+function draw() {
+  //clear canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  drawBall()
+  drawPaddle()
+  drawScore()
+  drawBricks()
 }
 
 //update canvas drawing and animation
